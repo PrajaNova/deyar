a0db9fec7025bd08b0171e60fb3f589449d8f7c0
shainsethi <shainsethi1000@gmail.com>
2026-06-04 21:54:07 +0530

    Untrack .next directory and update .gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9mcv4DwECQwIPbaEcXgn/Jrk5M+XcQHsWmE3+jTVUmM=\"\n}"
--- .next/static/chunks/src_app_menu_page_tsx_fb8b232c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_7ae9a068._.js"
-],
-    source: "dynamic"
-});
